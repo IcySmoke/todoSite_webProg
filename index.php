621ab48e26dd21b6418111ec224770ebea3b00db
Kozma Oliver <kozma.oliver@gmail.com>
2019-12-04 12:42:05 +0100

    base login page + base css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="hu">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Todo</title>
+  </head>
+  <header>
+    <h1>Todo Site</h1>
+  </header>
+  <body>
+    <div class="login">
+      <form class="loginForm" action="index.php" method="post">
+        <input type="text" name="username" placeholder="username" required autofocus>
+        <input type="password" name="password" placeholder="password" required>
+        <button class="login-btn" type="submit" name="login">Login</button>
+      </form>
+    </div>
+  </body>
+</html>
